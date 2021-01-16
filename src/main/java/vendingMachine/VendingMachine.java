b2b339f4214ecdc29a86334064c69000ec14c18f
ShervilG <wintergpt45@gmail.com>
2021-01-17 02:19:04 +0530

    first commit

---
+++ src/main/java/vendingMachine/VendingMachine.java
@@ -0,0 +1,11 @@
+package main.java.vendingMachine;
+
+/*
+A code to implement a Vending machine that has a bunch of products like
+chocolates, candy, cold-drink, and accept some coins.
+On inserting a coin, get a product back, and get the change back.
+Unit test to demonstrate that these common use cases work.
+*/
+
+public class VendingMachine {
+}
